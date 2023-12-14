f01680efce94fb5254893de3b9e1e2c5be03ae82
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-14 13:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function mapLetters(word) {
+    let obj = {};
+    for (let i = 0; i < word.length; i++) {
+        let arr = [];
+        for (let j = 0; j < word.length; j++) {
+            if (word[i] == word[j]) {
+                arr.push(j);
+            }
+        }
+        obj[word[i]] = arr; 
+	
+    }
+    return obj;
+}
+// const word = prompt("Enter word");
+// alert(mapLetters(word));
+
+const word = prompt("Enter word");
+const result = mapLetters(word);
+alert(JSON.stringify(result));
